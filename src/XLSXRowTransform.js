838f5aa8759e3be765e8b20eed34304c8d25eac7
Jule Marcoueille <jule.marcoueille@gmail.com>
2020-03-26 19:48:00 +0100

    feat(input): Add headers and object handling as input row

--- src/XLSXRowTransform.js
+++ src/XLSXRowTransform.js
@@ -8,22 +8,25 @@ export default class XLSXRowTransform extends Transform {
   /**
    * Create new xlsx row transform stream
    * @param {Object} [options]
-   * @param {Boolean} [options.format=true] - If set to false writer will not format cells with number, date, boolean and text. Default: true.
+   * @param {Boolean} [options.headers=false] - If set to true writer will output first row with an header style.
+   * @param {Boolean} [options.format=true] - If set to false writer will not format cells with number, date, boolean and text.
    * @param {Styles} [options.styles=new Styles()] - If set you can overwrite default standard type styles by other standard ones or even define custom `formatCode`.
    */
-  constructor({ format = true, styles = new Styles() }) {
+  constructor({ headers = false, format = true, styles = new Styles() }) {
     super({ objectMode: true });
+
     this.rowCount = 0;
+    this.headers = headers;
     this.format = format;
     this.styles = styles;
+
+    if (this.headers) throw new Error('Headers special style output not yet implemented.');
   }
 
   /**
    * Transform array to row string
    */
   _transform(row, encoding, callback) {
-    if (!Array.isArray(row)) return callback(); // TODO: Handle row as Object with column headers as keys
-
     const xlsxRow = Row(this.rowCount, row, this.format, this.styles);
     this.rowCount++;
     callback(null, xlsxRow);
--- src/XLSXWriteStream.js
+++ src/XLSXWriteStream.js
@@ -1,17 +1,24 @@
+import defaultsDeep from 'lodash/defaultsDeep';
 import Archiver from 'archiver';
 import { Transform, PassThrough } from 'stream';
 import * as templates from './templates';
 import XLSXRowTransform from './XLSXRowTransform';
+
+const isBoolean = value => value === !!value;
 
 /**
  * XLSX Write Stream base class
  */
 export default class XLSXWriteStream extends Transform {
   /**
-   * Create new Stream
+   * Create new stream transform that handles Array or Object as input chunks.
+   * Be aware that first row chunk is determinant in the transform configuration process for further row chunks.
    * @class XLSXWriteStream
    * @extends Transform
    * @param {Object} [options]
+   * @param {Array|Boolean} [options.headers=false] - If set to an array they will be printed in first row, no matter what is streamed in input.
+   *                                                  If receiving objects from input stream, only properties given in headers will be printed following headers order.
+   *                                                  If set to true it only has effect when streaming objects in order to print inferred headers in first place.
    * @param {Boolean} [options.format=true] - If set to false writer will not format cells with number, date, boolean and text.
    * @param {Object} [options.styleDefs] - If set you can overwrite default standard type styles by other standard ones or even define custom `formatCode`.
    * @param {Boolean} [options.immediateInitialization=false] - If set to true writer will initialize archive and start compressing xlsx common stuff immediately, adding subsequently a little memory and processor footprint. If not, initialization will be delayed to the first data processing.
@@ -19,17 +26,41 @@ export default class XLSXWriteStream extends Transform {
   constructor(options) {
     super({ objectMode: true });
 
+    this.pipelineInitialized = false;
     this.initialized = false;
+    this.arrayMode = null;
 
-    this.options = Object.assign({ format: true, immediateInitialization: false }, options);
+    this.options = defaultsDeep({}, options, { headers: false, format: true, immediateInitialization: false });
 
     if (this.options.immediateInitialization) this._initializePipeline();
+  }
+
+  _transform(chunk, encoding, callback) {
+    if (!this.initialized) this._initialize(chunk);
+
+    this.toXlsxRow.write(this.normalize(chunk), encoding, callback);
+  }
+
+  _initialize(chunk) {
+    this._initializePipeline();
+    this._initializeHeaders(chunk);
+
+    if (chunk) {
+      this.arrayMode = Array.isArray(chunk);
+      this.normalize = this.arrayMode
+        ? chunk => this.options.headers.map((value, index) => chunk[index])
+        : chunk => this.options.headers.map(key => chunk[key]);
+    }
+
+    this.initialized = true;
   }
 
   /**
    * Initialize pipeline with xlsx archive common files
    */
   _initializePipeline() {
+    if (this.pipelineInitialized) return;
+
     this.zip = Archiver('zip', { forceUTC: true });
     this.zip.catchEarlyExitAttached = true;
 
@@ -48,7 +79,7 @@ export default class XLSXWriteStream extends Transform {
       .on('warning', err => this.emit('warning', err))
       .on('error', err => this.error('error', err));
 
-    this.toXlsxRow = new XLSXRowTransform({ ...this.options, styles });
+    this.toXlsxRow = new XLSXRowTransform({ format: this.options.format, styles });
     this.sheetStream = new PassThrough();
     this.sheetStream.write(templates.SheetHeader);
     this.toXlsxRow.pipe(this.sheetStream);
@@ -56,16 +87,33 @@ export default class XLSXWriteStream extends Transform {
       name: 'xl/worksheets/sheet1.xml'
     });
 
-    this.initialized = true;
+    this.pipelineInitialized = true;
   }
 
-  _transform(chunk, encoding, callback) {
-    if (!this.initialized) this._initializePipeline();
-    this.toXlsxRow.write(chunk, encoding, callback);
+  _initializeHeaders(chunk = []) {
+    const shouldPrintHeaders = !!this.options.headers;
+
+    if (Array.isArray(chunk)) {
+      if (isBoolean(this.options.headers)) {
+        // Cannot infer headers from an Array
+        // => Ignore `headers` option silently.
+        this.options.headers = chunk.map(() => undefined);
+        return;
+      }
+    } else {
+      if (isBoolean(this.options.headers)) {
+        // Init headers from chunk
+        this.options.headers = [...Object.keys(chunk)];
+      }
+    }
+
+    if (shouldPrintHeaders) {
+      this.toXlsxRow.write(this.options.headers);
+    }
   }
 
   _flush(callback) {
-    if (!this.initialized) this._initializePipeline();
+    if (!this.initialized) this._initialize();
     this._finalize().then(() => {
       callback();
     });
